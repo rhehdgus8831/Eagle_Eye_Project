e5d572381beb312b6e3d12352bf91d46ed3d6743
rhehdgus8831 <rhehdgus8831@naver.com>
2025-09-03 20:50:56 +0900

    Initial commit

---
+++ backend/src/test/java/eagle/eye/dashboard/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eagle.eye.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>main page</h1>
+    </>
+  )
+}
+
+export default App
